09584341be21b64c98eccfe700ca438e24c14e83
selvadevel <selvadevel@gmail.com>
2015-05-20 08:00:28 +0530

    Update table.php

--- application/views/admin/table.php
+++ application/views/admin/table.php
@@ -62,7 +62,7 @@
                   	</div>
                   <div class="col-xs-3">
                   		<div class="dataTables_length dataTables_filter">
-                  			<select name="example1_length" size="1" class="form-control"  aria-controls="example1">
+                  			<select name="example1_length" size="1" class="form-control"  aria-controls="example1" id="limit">
                   				<option value="10" selected="selected">10</option>
                   				<option value="25">25</option><option value="50">50</option>
                   				<option value="100">100</option>
@@ -73,22 +73,47 @@
                   		</div>
                   	</div>
                   </div>
+                  <?php 
+                  
+						$thead="";
+
+						if(array_key_exists('check-box',$this->tbl_config_field)){
+
+							$thead.='<th><input type="checkbox" id="selectAll"></th>';
+
+							unset($this->tbl_config_field['check_box']);
+
+						}
+						
+						$i=1;
+						
+						foreach($this->tbl_config_field as $key => $value){
+							//($key);
+							$temp = $i==1 ? '<th class="asc"><a href="#" onclick="return false" data-field="'.$key.'" data-direction="asc">'.$value.'</th>' : '<th><a href="#" onclick="return false" data-field="'.$key.'">'.$value.'</th>';
+							
+							$thead.=$temp;
+							
+							$i++;
+							
+						}
+                   ?>
                   <table class="table table-bordered" id="itemList">
                   	<thead>
 	                    <tr>
-	                      <th><input type="checkbox" id="selectAll"></th>
-	                      <th class="sorting_asc"><a href="#" data-field="companyname" data-direction="asc">Company<i class="icon-arrow-up-3"></i></a></th>
-	                      <th><a href="#" data-field="create_date">Created_date</th>
-	                      
+	                      <!--<th><input type="checkbox" id="selectAll"></th>
+	                      <th class="asc"><a href="#" onclick="return false" data-field="id" data-direction="asc">ID<i class="icon-arrow-up-3"></i></a></th>
+	                      <th><a href="#" onclick="return false" data-field="create_date">Company</th>
+	                      <th><a href="#" onclick="return false" data-field="create_date">parent Name</th>-->
+	                      <?php echo $thead;?>
 	                    </tr>
                     </thead>
                     <tbody>
-                    	<?php echo $tbl;?>
+                    
                     </tbody>
                   </table>
                 </div><!-- /.box-body -->
-                <div class="box-footer clearfix">
-                  <ul class="pagination pagination-sm no-margin pull-right">
+                <div class="box-footer clearfix" id="results">
+                  <ul id="results" class="pagination pagination-sm no-margin pull-right">
                     <li><a href="#">&laquo;</a></li>
                     <li><a href="#">1</a></li>
                     <li><a href="#">2</a></li>
@@ -110,23 +135,107 @@
     <script>
     	$(document).ready(function(){
     		site_url='<?php echo site_url();?>';
-    		$.ajax({
-			    url : site_url+"/admin/table",
-			    type: "POST",
-			    data : {order:"asc"},
-			    success: function(data, textStatus, jqXHR)
-			    {
-			        //data - response from server
-			    }
-			});
 			$('#selectAll').click (function () {
 			     var checkedStatus = this.checked;
 			    $('#itemList tbody tr').find('td:first :checkbox').each(function () {
 			        $(this).prop('checked', checkedStatus);
 			     });
 			});
-			$("#itemList thead tr th a").click(function(){
-				alert()
-			});
     	});
+    </script>
+    
+     <script>
+        $(document).ready(function(){
+            site_url='<?php echo site_url();?>';
+            $('#selectAll').click (function () {
+                 var checkedStatus = this.checked;
+                $('#itemList tbody tr').find('td:first :checkbox').each(function () {
+                    $(this).prop('checked', checkedStatus);
+                 });
+            });
+            $(document).on("click","#itemList thead tr th a",function(){
+               className=$(this).closest("th").attr("class");
+               $($(this).closest("thead").find("th")).each(function(){
+                   $(this).attr("class","");
+               });
+               if(className=="asc"){
+               	$(this).closest("th").addClass("desc");
+			   	 $(this).closest("th").find("a").attr("data-direction","desc");
+			   }
+               else if(className=="desc"){
+               	$(this).closest("th").addClass("asc");
+               	 $(this).closest("th").find("a").attr("data-direction","asc");
+			   }
+               else{
+               	$(this).closest("th").addClass("asc");
+			   	$(this).closest("th").find("a").attr("data-direction","asc");
+			   }
+               post_data();
+            });
+            $(document).on("change","#limit",function(){
+                    post_data();
+                    //$("#results").on( "click", ".pagination a");
+            })
+            function post_data(limit_start=false){
+                $("#itemList thead tr th").each(function(){
+                    active_class=$(this).attr("class");
+                    if(active_class!="" && active_class!==undefined){
+                        order_by=$(this).find("a").attr("data-direction");
+                        order_by_name=$(this).find("a").attr("data-field");
+                    }
+                });
+                limit=$("#limit").val();
+                if(limit_start==false)
+                limit_start=$(".pagination").find(".active a").text();
+                trHTML="";
+                $.ajax({
+                    url : site_url+"/admin/table/table_response",
+                    type: "POST",
+                    data : {order_by_name:order_by_name,order_by:order_by,limit:limit,limit_start:limit_start},
+                    success: function(data)
+                    {
+                        trHTML="";
+                        obj = JSON.parse(data);
+                        $("#results").html(obj.pagination);
+                        table=obj.table;
+                        $.each(table, function (i, item) {
+                            trHTML += '<tr>';
+                            trHTML += '<td><input type="checkbox" id="selectAll"></td><td>' + item.id + '</td><td>' + item.name + '</td><td>' + item.parent + '</td>';
+                            trHTML += '</tr>';
+                        });
+                        $("#itemList tbody").html(trHTML);
+                    }
+                });
+            }
+        $.ajax({
+             url : site_url+"/admin/table/table_response",
+             type: "POST",
+             data:{"limit_start":"1","limit":"10",'order_by':"asc",'order_by_name':"name"},
+             success: function(data){
+                trHTML="";
+                obj = JSON.parse(data);
+                $("#results").html(obj.pagination);
+                table=obj.table;
+                $.each(table, function (i, item) {
+                	trHTML += '<tr>';
+                	$.each(table, function (j, itemm) {
+                		console.log(itemm);
+                		/*if(itemm.check_box=="check_box")
+                		trHTML += '<td><input type="checkbox" id="selectAll"></td>';
+                		else
+                		console.log(itemm);*/
+                	});
+                	trHTML += '</tr>';
+                   // trHTML += '<td><input type="checkbox" id="selectAll"></td><td>' + item.id + '</td><td>' + item.name + '</td><td>' + item.parent + '</td>';
+                   
+                });
+                $("#itemList tbody").html(trHTML);
+            }
+        });
+        $("#results").on("click",".pagination a",function(){
+            var page = $(this).attr("data-page");
+            post_data(page);
+        });
+
+        });
     </script>
